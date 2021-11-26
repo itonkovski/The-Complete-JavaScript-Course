0b226686ce7e6853e1efd3ea58bf6473530dc014
Ivaylo Tonkovski <65118301+itonkovski@users.noreply.github.com>
2021-11-26 16:01:52 +0100

    Fundamentals part2, first challenge and lessons upload

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -34,6 +34,7 @@ console.log(describeNorway);
 // 'percentageOfWorld2', and also call it with 3 country populations (can be
 // the same populations)
 
+/*
 function percentageOfWorld1(population) {
     const percentage = (population / 7900) * 100;
     return percentage;
@@ -49,3 +50,87 @@ const secondPercentage = percentageOfWorld1(140).toFixed(2);
 const thirdPercentage = percentageOfWorld1(140).toFixed(2);
 
 console.log(firstPercentage, secondPercentage, thirdPercentage);
+*/
+
+// LECTURE: Arrow Functions
+// 1. Recreate the last assignment, but this time create an arrow function called
+// 'percentageOfWorld3'
+
+/*
+const percentageOfWorld = population => (population / 7900) * 100;
+
+const firstPercentage = percentageOfWorld(10).toFixed(2);
+const secondPercentage = percentageOfWorld(140).toFixed(2);
+const thirdPercentage = percentageOfWorld(140).toFixed(2);
+
+console.log(firstPercentage, secondPercentage, thirdPercentage);
+*/
+
+// LECTURE: Functions Calling Other Functions
+// 1. Create a function called 'describePopulation'. Use the function type you
+// like the most. This function takes in two arguments: 'country' and
+// 'population', and returns a string like this: 'China has 1441 million people,
+// which is about 18.2% of the world.'
+// 2. To calculate the percentage, 'describePopulation' call the
+// 'percentageOfWorld1' you created earlier
+// 3. Call 'describePopulation' with data for 3 countries of your choice
+
+/*
+const percentageOfWorld = population => ((population / 7900) * 100).toFixed(2);
+
+function describePopulation(country, population) {
+    const percentage = percentageOfWorld(population);
+    const result = `${country} has ${population} million people, which is about ${percentage}% of the world.`
+    return result;
+}
+
+const finalResult = describePopulation('Norway', 10);
+
+console.log(finalResult);
+*/
+
+// Coding Challenge #1
+// Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new
+// gymnastics discipline, which works differently.
+// Each team competes 3 times, and then the average of the 3 scores is calculated (so
+// one average score per team).
+// A team only wins if it has at least double the average score of the other team.
+// Otherwise, no team wins!
+// Your tasks:
+// 1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
+// 2. Use the function to calculate the average for both teams
+// 3. Create a function 'checkWinner' that takes the average score of each team
+// as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner
+// to the console, together with the victory points, according to the rule above.
+// Example: "Koalas win (30 vs. 13)"
+// 4. Use the 'checkWinner' function to determine the winner for both Data 1 and
+// Data 2
+// 5. Ignore draws this time
+// Test data:
+// § Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
+// § Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+// Hints:
+// § To calculate average of 3 values, add them all together and divide by 3
+// § To check if number A is at least double number B, check for A >= 2 * B.
+// Apply this to the team's average scores �
+// GOOD LUCK 
+
+const firstTeam = 'Dolphins';
+const secondTeam = 'Koalas';
+
+const calcAverage = (firstScore, secondScore, thirdScore) => (firstScore + secondScore + thirdScore) / 3;
+
+const scoreDolphins = calcAverage(44, 23, 71);
+const scoreKoalas = calcAverage(65, 54, 49);
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+    if (avgDolphins >= 2 * avgKoalas) {
+        console.log(`${firstTeam} wins (${avgDolphins} vs. ${avgKoalas})`);
+    } else if (avgKoalas >= 2 * avgDolphins) {
+        console.log(`${secondTeam} wins (${avgKoalas} vs. ${avgDolphins})`);
+    } else {
+        console.log(`Draw. Play again.`);
+    }
+}
+
+checkWinner(scoreDolphins, scoreKoalas);
