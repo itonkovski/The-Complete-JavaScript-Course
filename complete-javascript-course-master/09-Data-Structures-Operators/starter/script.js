66021a7abf7ef69b6bb18ea5403473bb55fe3f7b
Ivaylo Tonkovski <65118301+itonkovski@users.noreply.github.com>
2022-02-07 21:48:34 +0100

    coding challenge 4 upload

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -280,6 +280,7 @@ console.log(answer);
 console.log(quizz.get(quizz.get('correct') === answer));
 */
 
+/*
 // Coding Challenge #3
 // Let's continue with our football betting app! This time, we have a map called 'gameEvents' (see below) with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
 // Your tasks:
@@ -322,3 +323,65 @@ for (const [min, event] of gameEvents) {
   const half = min <= 45 ? '[FIRST HALF]' : '[SECOND HALF]';
   console.log(`${half} ${min} : ${event}`);
 }
+*/
+
+/*
+//Working with strings
+
+const maskingCreditCard = function (number) {
+  const numToString = String(number);
+  const last4Digits = numToString.slice(-4);
+  return last4Digits.padStart(numToString.length, '*');
+};
+
+console.log(maskingCreditCard(2345434434567678));
+console.log(maskingCreditCard('2345434434567678'));
+*/
+
+// Coding Challenge #4
+
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+*/
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+  const rows = text.split('\n');
+
+  for (const [i, row] of rows.entries()) {
+    const [first, second] = row.toLowerCase().trim().split('_');
+    const outPut = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    console.log(`${outPut.padEnd(20)}${'✅'.repeat(i + 1)}`);
+  }
+});
