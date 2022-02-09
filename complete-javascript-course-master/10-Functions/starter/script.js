3ae4b48cc3869e5e0c9ea37e64cb2244ac19c92b
Ivaylo Tonkovski <65118301+itonkovski@users.noreply.github.com>
2022-02-09 20:20:34 +0100

    Functions Accepting Callback Functions

--- complete-javascript-course-master/10-Functions/starter/script.js
+++ complete-javascript-course-master/10-Functions/starter/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+// Functions Accepting Callback Functions
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [firstWord, ...others] = str.split(' ');
+  return [firstWord.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transform = function (str, fun) {
+  console.log(`Original string => ${str}`);
+  console.log(`One word transformation => ${fun(str)}`);
+  console.log(`Upper first transformation => ${fun(str)}`);
+};
+
+transform('Lorem ipsum dolor sit amet', oneWord);
+transform('Lorem ipsum dolor sit amet', upperFirstWord);
