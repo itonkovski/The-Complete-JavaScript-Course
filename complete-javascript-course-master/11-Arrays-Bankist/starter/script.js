1ff78818dda8b069e309d6206e53f846866ef2fe
Ivaylo Tonkovski <65118301+itonkovski@users.noreply.github.com>
2022-03-22 21:31:35 +0100

    Arrays - foreach with maps

--- complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -63,14 +63,27 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
+/////////////////////////////////////////////////
 // LECTURES
 
+// Looping Arrays: forEach
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+movements.forEach(function (mov, i) {
+  if (mov > 0) {
+    console.log(`Movement ${i + 1} : You deposited ${mov}`);
+  } else {
+    console.log(`Movement ${i + 1} : You withdrew ${Math.abs(mov)}`);
+  }
+});
+
+// forEach With Maps
 const currencies = new Map([
   ['USD', 'United States dollar'],
   ['EUR', 'Euro'],
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-/////////////////////////////////////////////////
+currencies.forEach(function (value, key, map) {
+  console.log(`${key} : ${value}`);
+});
