8ba9f8f0ff090e259e3d302d9a6a61e261d82e5f
Ivaylo Tonkovski <65118301+itonkovski@users.noreply.github.com>
2022-03-22 20:18:46 +0100

    challenge 2 Functions

--- complete-javascript-course-master/10-Functions/starter/script.js
+++ complete-javascript-course-master/10-Functions/starter/script.js
@@ -126,6 +126,7 @@ console.log(AddVATNew(100));
 
 // GOOD LUCK 😀
 
+/*
 const poll = {
   question: 'What is your favourite programming language?',
   options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
@@ -168,3 +169,23 @@ poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
 
 poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
 poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
+*/
+
+// Coding Challenge #2
+
+// This is more of a thinking challenge than a coding challenge 🤓
+
+// Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+
+// And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+
+// GOOD LUCK
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.addEventListener('click', function () {
+    header.style.color = 'yellow';
+  });
+})();
